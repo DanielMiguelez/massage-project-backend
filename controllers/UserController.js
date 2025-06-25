26331e76796d9a6ae16e366280eee3742efc4c86
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-06-25 17:16:51 +0200

    create user, and find users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,6 +11,24 @@ const UserController = {
         } catch(error) {
             console.log("Not able to create user:", error);
         }
+    },
+
+    async getAllUsers(req, res){
+        try {
+            const users = await User.find();
+            res.status(201).send({msg:"Users brought successfully" , users})
+        } catch (error) {
+            console.log("Not able to bring users:", error);
+        }
+    },
+
+    async getUserById(req, res){
+        try {
+            const userById = await User.findById(req.params._id);
+            res.status(201).send({msg:"User bythe ID you selected" , userById})
+        } catch (error) {
+            console.log("Not able to bring user:", error);
+        }
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const UserController = require("../controllers/UserController")
 
 router.post("/createUser", UserController.createUser)
+router.get("/getAllUsers", UserController.getAllUsers)
+router.get("/getUserById/:_id", UserController.getUserById)
 
 module.exports = router;
