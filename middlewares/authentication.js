d01a93ab024425be9186cd40562b96922f3e02a0
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-06-26 19:16:43 +0200

    login created

---
+++ middlewares/authentication.js
@@ -0,0 +1,35 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+
+const authentication = async(req, res, next) =>{
+    try{
+        const authorizationHeader = req.headers.authorization;
+
+        if(!authorizationHeader){
+            return res.status(401).send({ msg: "No token provided" });
+        }
+
+        const token = authorizationHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).send({ msg: "Token malformed" });
+        }
+
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(payload.userId);
+
+        if (!user) {
+            return res.status(401).send({ msg: "You have no permission" });
+        }
+
+        req.user = user;
+        next();
+
+    }catch(error){
+        console.error(error);
+        return res.status(500).send({ error, message: 'There was a problem with the token' });
+    }
+}
+
+module.exports = { authentication }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../controllers/UserController")
+const { authentication } = require("../middlewares/authentication")
 
 router.post("/createUser", UserController.createUser)
-router.get("/getAllUsers", UserController.getAllUsers)
+router.get("/getAllUsers", authentication, UserController.getAllUsers)
 router.get("/getUserById/:_id", UserController.getUserById)
 router.delete("/deleteUserById/:_id", UserController.deleteUserById)
 router.put("/updateUserById/:_id", UserController.updateUserById)
